b611d780961f4de46fdddc8a068043fcdd52d297
Jherly <39920886+Jherly@users.noreply.github.com>
2018-08-01 14:24:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function mudarCor1(){
+	var mudarCor1 = document.getElementById('div1');
+	mudarCor1.style.background="#FD8D31";
+}
+function voltarCor1(){
+	var voltarCor1 = document.getElementById('div1');
+	voltarCor1.style.background="#5BEB53";
+}
+
+function mudarCor2(){
+	var mudarCor2 = document.getElementById('div2');
+	mudarCor2.style.background="#5BEB53";
+}
+function voltarCor2(){
+	var voltarCor2 = document.getElementById('div2');
+	voltarCor2.style.background="#FD8D31";
+}
+
+function mudarCor3(){
+	var mudarCor3 = document.getElementById('div3');
+	mudarCor3.style.background="#86DCF8";
+}
+function voltarCor3(){
+	var voltarCor3 = document.getElementById('div3');
+	voltarCor3.style.background="#FA7777";
+}
+
+function mudarCor4(){
+	var mudarCor4 = document.getElementById('div4');
+	mudarCor4.style.background="#FA7777";
+}
+function voltarCor4(){
+	var voltarCor4 = document.getElementById('div4');
+	voltarCor4.style.background="#86DCF8";
+}
